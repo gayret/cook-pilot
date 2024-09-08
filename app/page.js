46baccde04f91bad2bbe28943d0c9ee2d62e9599
safa gayret <safagayret@gmail.com>
2024-09-08 19:34:39 +0300

    first commit

---
+++ app/page.js
@@ -0,0 +1,13 @@
+export default function Home() {
+  
+
+  return (
+   <div id="app-wrapper">
+      <section id="foods">
+        <ul>
+          <li></li>
+        </ul>
+      </section>
+   </div>
+  );
+}
